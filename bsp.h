179fc42231cf4988e105817ae234825cd1789c97
David R. Piegdon <dgit@piegdon.de>
2020-05-03 02:07:25 +0000

    cleanup and use BSP

--- bsp.h
+++ bsp.h
@@ -4,32 +4,39 @@
 #include "nrfx_gpiote.h"
 #include "nrfx_uarte.h"
 
-const nrfx_gpiote_pin_t pin_ocxo_vref = NRF_GPIO_PIN_MAP(0, 2);
-const nrfx_gpiote_pin_t pin_dac_value = NRF_GPIO_PIN_MAP(0, 3);
-const nrfx_gpiote_pin_t pin_gps_txd = NRF_GPIO_PIN_MAP(0, 4);
+const nrfx_gpiote_pin_t pin_fault_led = NRF_GPIO_PIN_MAP(0, 8);
+
+const nrfx_gpiote_pin_t pin_pwr_en_1v2 = NRF_GPIO_PIN_MAP(0, 9);
+const nrfx_gpiote_pin_t pin_pwr_en_12v = NRF_GPIO_PIN_MAP(0, 10);
+const nrfx_gpiote_pin_t pin_pwr_nen_10v = NRF_GPIO_PIN_MAP(0, 17);
+const nrfx_gpiote_pin_t pin_pwr_good = NRF_GPIO_PIN_MAP(1, 6);
+
 const nrfx_gpiote_pin_t pin_i2c_scl = NRF_GPIO_PIN_MAP(0, 5);
 const nrfx_gpiote_pin_t pin_i2c_sda = NRF_GPIO_PIN_MAP(0, 6);
-const nrfx_gpiote_pin_t pin_gps_nreset = NRF_GPIO_PIN_MAP(0, 7);
-const nrfx_gpiote_pin_t pin_fault_led = NRF_GPIO_PIN_MAP(0, 8);
-const nrfx_gpiote_pin_t pin_en_1v2 = NRF_GPIO_PIN_MAP(0, 9);
-const nrfx_gpiote_pin_t pin_en_12v = NRF_GPIO_PIN_MAP(0, 10);
-const nrfx_gpiote_pin_t pin_gps_rxd = NRF_GPIO_PIN_MAP(0, 12);
-const nrfx_gpiote_pin_t pin_pll_nen = NRF_GPIO_PIN_MAP(0, 13);
-const nrfx_gpiote_pin_t pin_fpga_int = NRF_GPIO_PIN_MAP(0, 15);
-const nrfx_gpiote_pin_t pin_nen_10v = NRF_GPIO_PIN_MAP(0, 17);
-const nrfx_gpiote_pin_t pin_pll_int = NRF_GPIO_PIN_MAP(0, 20);
-const nrfx_gpiote_pin_t pin_dac_nalert = NRF_GPIO_PIN_MAP(0, 22);
-const nrfx_gpiote_pin_t pin_dac_nclear = NRF_GPIO_PIN_MAP(0, 24);
-const nrfx_gpiote_pin_t pin_fpga_wdt = NRF_GPIO_PIN_MAP(0, 26);
-const nrfx_gpiote_pin_t pin_fpga_ncs = NRF_GPIO_PIN_MAP(0, 28);
+
 const nrfx_gpiote_pin_t pin_spi_sdo = NRF_GPIO_PIN_MAP(0, 29);
+const nrfx_gpiote_pin_t pin_spi_clk = NRF_GPIO_PIN_MAP(1, 10);
+const nrfx_gpiote_pin_t pin_spi_sdi = NRF_GPIO_PIN_MAP(1, 13);
+const nrfx_gpiote_pin_t pin_spi_ncs_fpga = NRF_GPIO_PIN_MAP(0, 28);
+const nrfx_gpiote_pin_t pin_spi_ncs_dac = NRF_GPIO_PIN_MAP(1, 11);
+
 const nrfx_gpiote_pin_t pin_fpga_nreset = NRF_GPIO_PIN_MAP(0, 30);
 const nrfx_gpiote_pin_t pin_fpga_cdone = NRF_GPIO_PIN_MAP(0, 31);
+const nrfx_gpiote_pin_t pin_fpga_wdt = NRF_GPIO_PIN_MAP(0, 26);
+const nrfx_gpiote_pin_t pin_fpga_int = NRF_GPIO_PIN_MAP(0, 15);
+
+const nrfx_gpiote_pin_t pin_dac_value = NRF_GPIO_PIN_MAP(0, 3);
+const nrfx_gpiote_pin_t pin_dac_nalert = NRF_GPIO_PIN_MAP(0, 22);
+const nrfx_gpiote_pin_t pin_dac_nclear = NRF_GPIO_PIN_MAP(0, 24);
 const nrfx_gpiote_pin_t pin_dac_nreset = NRF_GPIO_PIN_MAP(1, 2);
 const nrfx_gpiote_pin_t pin_dac_nload = NRF_GPIO_PIN_MAP(1, 4);
-const nrfx_gpiote_pin_t pin_pgood = NRF_GPIO_PIN_MAP(1, 6);
-const nrfx_gpiote_pin_t pin_gps_pulse = NRF_GPIO_PIN_MAP(1, 9);
-const nrfx_gpiote_pin_t pin_spi_clk = NRF_GPIO_PIN_MAP(1, 10);
-const nrfx_gpiote_pin_t pin_dac_ncs = NRF_GPIO_PIN_MAP(1, 11);
-const nrfx_gpiote_pin_t pin_spi_sdi = NRF_GPIO_PIN_MAP(1, 13);
 
+const nrfx_gpiote_pin_t pin_gps_pulse = NRF_GPIO_PIN_MAP(1, 9);
+const nrfx_gpiote_pin_t pin_gps_txd = NRF_GPIO_PIN_MAP(0, 4);
+const nrfx_gpiote_pin_t pin_gps_nreset = NRF_GPIO_PIN_MAP(0, 7);
+const nrfx_gpiote_pin_t pin_gps_rxd = NRF_GPIO_PIN_MAP(0, 12);
+
+const nrfx_gpiote_pin_t pin_pll_nen = NRF_GPIO_PIN_MAP(0, 13);
+const nrfx_gpiote_pin_t pin_pll_int = NRF_GPIO_PIN_MAP(0, 20);
+
+const nrfx_gpiote_pin_t pin_ocxo_vref = NRF_GPIO_PIN_MAP(0, 2);
--- dpsgo.c
+++ dpsgo.c
@@ -5,6 +5,7 @@
 #include <nrfx_uarte.h>
 #include <nrf_802154.h>
 #include <nrf_temp.h>
+#include "nrf_nvmc.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <strings.h>
@@ -12,31 +13,20 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-const uint32_t led_pin = NRF_GPIO_PIN_MAP(0,8);
+#include "bsp.h"
 
 static void clockEventHandler(nrfx_clock_evt_type_t event)
 {
 	(void) event;
 }
 
-#define MAX_MESSAGE_SIZE 17
-#define CHANNEL          23
-// chan23 center is 2465MHz
-
 /* hardware configuration / muxing */
-
-static const uint32_t led = NRF_GPIO_PIN_MAP(1,13);
-
-static const nrfx_gpiote_out_config_t led_config = {
-	.init_state = GPIOTE_CONFIG_OUTINIT_Low,
-	.task_pin = false
-};
 
 const nrfx_uarte_t uart0 = NRFX_UARTE_INSTANCE(0);
 
 static const nrfx_uarte_config_t uart_config = {
-	.pseltxd = NRF_GPIO_PIN_MAP(1,10),
-	.pselrxd = NRF_GPIO_PIN_MAP(1,11),
+	.pseltxd = pin_gps_rxd,
+	.pselrxd = pin_gps_txd,
 	.pselcts = NRF_UARTE_PSEL_DISCONNECTED,
 	.pselrts = NRF_UARTE_PSEL_DISCONNECTED,
 	.hwfc = NRFX_UART_DEFAULT_CONFIG_HWFC,
@@ -45,14 +35,47 @@ static const nrfx_uarte_config_t uart_config = {
 	.interrupt_priority = NRFX_UART_DEFAULT_CONFIG_IRQ_PRIORITY
 };
 
-/* global state variables */
+static inline void nop(void)
+{
+	__asm__ __volatile__("nop":::);
+}
 
-static volatile bool m_tx_done;
-static volatile bool m_tx_failed;
-static volatile nrf_802154_tx_error_t m_tx_errorcode;
+void setupUICR(void)
+{
+	unsigned changes = 0;
+
+	// make sure we use NFC pins as standard IO
+	if((NRF_UICR->NFCPINS & UICR_NFCPINS_PROTECT_Msk) != (UICR_NFCPINS_PROTECT_Disabled << UICR_NFCPINS_PROTECT_Pos)) {
+		nrf_nvmc_write_word((uint32_t)&NRF_UICR->NFCPINS,
+				(0xffffffff & ~UICR_NFCPINS_PROTECT_Msk)
+				| (UICR_NFCPINS_PROTECT_Disabled << UICR_NFCPINS_PROTECT_Pos));
+		++changes;
+	}
+
+	if((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) != (UICR_REGOUT0_VOUT_3V3 << UICR_REGOUT0_VOUT_Pos)) {
+		nrf_nvmc_write_word((uint32_t)&NRF_UICR->REGOUT0,
+				(0xffffffff & ~UICR_REGOUT0_VOUT_Msk)
+				| (UICR_REGOUT0_VOUT_3V3 << UICR_REGOUT0_VOUT_Pos));
+		++changes;
+	}
+
+	if(changes) {
+		while(1) {
+			// loop until reset and indicate state on fault LED
+			nrf_gpio_pin_toggle(pin_fault_led);
+			for(uint32_t i = 0; i < 0x50000; ++i)
+				nop();
+		}
+	}
+}
 
 static void init_hardware(void)
 {
+	nrfx_gpiote_init();
+	nrf_gpio_cfg_output(pin_fault_led);
+
+	setupUICR();
+
 	nrf_temp_init();
 
 	// setup RTC for FreeRTOS.
@@ -62,7 +85,16 @@ static void init_hardware(void)
 	nrf_clock_lf_src_set((nrf_clock_lfclk_t)NRFX_CLOCK_CONFIG_LF_SRC);
 	nrfx_clock_lfclk_start();
 
-	nrf_gpio_cfg_output(led_pin);
+
+	// disable all power rails
+	nrf_gpio_cfg_output(pin_pwr_nen_10v);
+	nrf_gpio_pin_set(pin_pwr_nen_10v);
+	nrf_gpio_cfg_output(pin_pwr_en_1v2);
+	nrf_gpio_pin_clear(pin_pwr_en_1v2);
+	nrf_gpio_cfg_output(pin_pwr_en_12v);
+	nrf_gpio_pin_clear(pin_pwr_en_12v);
+
+	nrf_gpio_cfg_input(pin_pwr_good, NRF_GPIO_PIN_NOPULL);
 }
 
 static void ledFlasherTask(void * led)
@@ -70,7 +102,7 @@ static void ledFlasherTask(void * led)
 	vTaskDelay(16 * (0xf & (unsigned) led));
 	while(1) {
 		nrf_gpio_pin_toggle(*(const uint32_t *)led);
-		vTaskDelay(configTICK_RATE_HZ/2);
+		vTaskDelay(configTICK_RATE_HZ/3);
 	}
 }
 
@@ -82,7 +114,7 @@ int main(void)
 
 	init_hardware();
 
-	ret = xTaskCreate(ledFlasherTask, "led", 128, (void*)&led_pin, 3, &ledFlasher);
+	ret = xTaskCreate(ledFlasherTask, "led", 128, (void*)&pin_fault_led, 3, &ledFlasher);
 	assert(ret == pdPASS);
 
 	vTaskStartScheduler();
